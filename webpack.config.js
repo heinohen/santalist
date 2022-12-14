4705a1e974152039d3be72a8235d108c43bab7f6
Henrik Heinonen <hohein@utu.fi>
2022-12-14 12:48:38 +0200

    contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main-[contenthash].js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
- 
+    
+    plugins: 
+        [new HtmlWebpackPlugin()],
+    
     module: {
         rules: [
             {
